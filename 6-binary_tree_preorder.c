7a1fb91d7a329578bb3ebb398ef99ca739152a67
Aalaa <alaamglal@gmail.com>
2024-03-27 15:28:53 -0700

    done

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,21 +7,11 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	const binary_tree_t *l = tree;
-	const binary_tree_t *r = tree;
-
 	if (tree == NULL || func == NULL)
 	{
 		return;
 	}
-	while (l != NULL)
-	{
-		(*func)(l->n);
-		l = l->left;
-	}
-	while (r != NULL)
-	{
-		(*func)(r->n);
-		r = r->right;
-	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
